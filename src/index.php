be1a27095e86e9375e7afeea46104cec2fef1842
Mallta <brunopmalltaads@gmail.com>
2025-05-23 02:34:54 -0300

    fix: centralizar carregamento do autoload do Composer no index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once 'config/database.php';
 require_once __DIR__ . '/routes.php';
+
 
 $db = Database::connect();
 echo "Conexão com o banco de dados funcionando!";
