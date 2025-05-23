fd1a3048caacd6db6358349bbf423bf6d890a6a5
Mallta <brunopmalltaads@gmail.com>
2025-05-23 02:41:35 -0300

    feat(routes): definir rotas principais da API

--- src/routes.php
+++ src/routes.php
@@ -3,14 +3,16 @@
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/controllers/AuthController.php';
 require_once __DIR__ . '/core/Router.php';
+require_once __DIR__ . '/controllers/PostController.php';
 
 $db = Database::connect();
 $router = new Router($db);
 
-//  rotas
+
 $router->add('POST', '/register', AuthController::class, 'register');
 $router->add('POST', '/login', AuthController::class, 'login');
-
+//rota autenticada
+$router->add('POST', '/posts', PostController::class, 'create');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
